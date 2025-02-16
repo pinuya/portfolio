9bcce00e66dcb9e23d3dd6dcacfd50c7373074d6
Tifany <tifsnunes@gmail.com>
2025-02-16 19:52:33 -0300

    add: 404 fallback

---
+++ app/routes/$.tsx
@@ -0,0 +1,30 @@
+import { Link } from "@remix-run/react";
+import { ArrowLeft } from "lucide-react";
+
+export default function Fallback() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="flex">
+        <img src="/assets/anime-eto.gif" alt="eh to" />
+        <div>
+          <h1 className="text-6xl font-bold text-gray-900 mb-4">404</h1>
+          <p className="text-2xl text-gray-600 mb-8">Oops! Page not found</p>
+          <div className="max-w-md mx-auto mb-8">
+            <p className="text-gray-500">
+              A página que você procura pode ter sido removida, ter seu nome
+              alterado ou estar temporariamente indisponível.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <Link
+        to={"/"}
+        className="inline-flex gap-2 items-center px-4 py-2 text-sm font-medium text-foreground rounded-md "
+      >
+        <ArrowLeft className="w-5 h-5" />
+        Volte para inicio
+      </Link>
+    </div>
+  );
+}
